dcb07a0fbf67a1f61958ccd66b7332bc12f58d49
diaslilian <lilian.dias25@gmail.com>
2020-09-04 13:07:57 -0300

    revisao exercicios semana

--- S3Exercicio/exercicios-semana.js
+++ S3Exercicio/exercicios-semana.js
@@ -19,23 +19,42 @@ function potenciaDeDois() {
 
 potenciaDeDois();
 
+// OUTRA OPÇÃO, usando o while
+
+// function potenciaDeDois(numero) {
+//   let resultado = 1;
+
+//   while (numero > 0) {
+//     resultado *= 2;
+//     numero -= 1;
+//   }
+
+//   return resultado;
+// }
+
 // 2) Crie uma função que receba três números e determine se um número é maior que outro.
 // Exemplo:
 // retornaNumMaior(10, 20, 30) // 30
 
-function retornaNumMaior() {
-  const meuArray = [0, 10, 13, 2, 30, 28, 1];
-  let maior = 0;
+function retornaNumMaior(num1, num2, num3) {
+  let maior = num1;
 
-  for (let i = 0; i < meuArray.length; i++) {
-    if (meuArray[i] > maior) {
-      maior = meuArray[i];
+  if (num1 > num2) {
+    if (num1 > num3) {
+      maior = num1;
+    } else {
+      maior = num3;
     }
+  } else if (num2 > num3) {
+    maior = num2;
+  } else {
+    maior = num3;
   }
+
   console.log("O maior número é: " + maior);
 }
 
-retornaNumMaior();
+retornaNumMaior(10, 20, 30);
 
 // ***************
 
@@ -68,11 +87,54 @@ function numPrimo() {
 
 numPrimo();
 
+// OUTRA OPÇÃO
+
+// function numPrimo(num) {
+//   if (num === 1) {
+//     return true;
+//   }
+//   for (let i = 2; i < num; i++) {
+//     if (num % i === 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
 // Crie uma função que conte o número de vogais em uma `string` e retorne o número de vogais.
 // Exemplo:
 // function contaVogais("carro") // 2
 
+function contaVogais(str) {
+  const vogais = "aeiou";
+  const arrayMinusculas = str.toLowerCase();
+  let totalVogais = 0;
+
+  for (let i = 0; i < arrayMinusculas.length; i += 1) {
+    for (let j = 0; j < vogais.length; j += 1) {
+      if (arrayMinusculas[i] === vogais[j]) {
+        totalVogais += 1;
+      }
+    }
+  }
+
+  return totalVogais;
+}
+
+console.log(contaVogais("carro"));
 // Use estruturas de controle para inverter uma array de inteiros. A função deverá receber uma
 // array de números e retornar uma array com a ordem dos elementos invertida. Não é válido utilizar `arr.reverse()`.
 // Exemplo:
 // function inverteArray([1, 2, 3, 4]) // [4, 3, 2, 1]
+
+function inverteArray(array) {
+  const arrayInvertida = [];
+
+  for (let i = array.length - 1; i >= 0; i -= 1) {
+    arrayInvertida.push(array[i]);
+  }
+
+  return arrayInvertida;
+}
+
+console.log(inverteArray([1, 2, 3, 4]));
